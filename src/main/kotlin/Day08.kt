76f58dc2ae75de65443d2615c9bc5dd2bfa46158
Hywel Bennett <hywel@lardysoft.com>
2022-12-09 02:05:18 +0000

    Day 8 pt 2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -42,6 +42,52 @@ fun countVisibleTrees(input: String): Int {
     return visibleTrees
 }
 
+fun getBestScenicScore(input: String): Int {
+    var bestScore = 0
+    parseTrees(input).let { trees ->
+        for ((rowIndex, row) in trees.withIndex()) {
+            for ((columnIndex, column) in row.withIndex()) {
+                trees.getScenicScore(rowIndex, columnIndex).takeIf { it > bestScore }?.let { bestScore = it }
+            }
+        }
+    }
+    return bestScore
+}
+
+fun Array<IntArray>.getScenicScore(i: Int, j: Int): Int {
+    val height = this[i][j]
+    var northVisible = 0
+    var southVisible = 0
+    var westVisible = 0
+    var eastVisible = 0
+    for (northIndex in i - 1 downTo 0) {
+        northVisible += 1
+        if (this[northIndex][j] >= height) {
+            break
+        }
+    }
+    for (southIndex in i + 1 until this.size) {
+        southVisible += 1
+        if (this[southIndex][j] >= height) {
+            break
+        }
+    }
+    for (westIndex in j - 1 downTo 0) {
+        westVisible += 1
+        if (this[i][westIndex] >= height) {
+            break
+        }
+    }
+    for (eastIndex in j + 1 until this.size) {
+        eastVisible += 1
+        if (this[i][eastIndex] >= height) {
+            break
+        }
+    }
+    return northVisible * southVisible * westVisible * eastVisible
+
+}
+
 private fun parseTrees(input: String): Array<IntArray> {
     val rows = input.lines().filterNot { it.isBlank() }
     val columns = rows.first().length
@@ -59,6 +105,14 @@ private fun parseTrees(input: String): Array<IntArray> {
 }
 
 fun main() {
+//    val input = """
+//        30373
+//        25512
+//        65332
+//        33549
+//        35390
+//    """.trimIndent()
     val input = Utils.readResource("08.txt").trimIndent()
     countVisibleTrees(input).also { println(it) }
+    getBestScenicScore(input).also { println(it) }
 }
